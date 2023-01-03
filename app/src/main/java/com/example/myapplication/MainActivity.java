f2727fd635cbbea87da720a9a0e7251d4948e8b4
Antelope <ffstormff@gmail.com>
2023-01-03 21:31:10 +0700

    Delete fab in main2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -49,14 +49,6 @@ public class MainActivity extends AppCompatActivity  {
         setContentView(binding.getRoot());
         AnhXa();
         setSupportActionBar(binding.appBarMain.toolbar);
-
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navHeaderView;
 
